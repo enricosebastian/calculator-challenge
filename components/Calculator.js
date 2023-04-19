e1852344b2b8e2864d6b5c242f1c55aa9a3d04dc
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-19 22:45:07 +0800

    improved structure of component

--- components/Calculator.js
+++ components/Calculator.js
@@ -4,8 +4,14 @@ import styles from '@/styles/Home.module.scss'
 export default function Calculator() {
     return (
         <div className={styles.calculator__body__wrapper}>
+
             <div className={styles.calculator__equation__wrapper}>equation</div>
-            <div className={styles.calculator__answer__wrapper}>answer</div>
+            <div className={styles.calculator__input__wrapper}>input</div>
+            <div className={styles.calculator__buttons__wrapper}>
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+            </div>
         </div>
     );
 }
