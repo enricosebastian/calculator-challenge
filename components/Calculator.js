af9f094e3b0fa58d3de4b694c0eeda16a3c70bf8
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-19 23:00:52 +0800

    integrated actual colors

--- components/Calculator.js
+++ components/Calculator.js
@@ -8,9 +8,99 @@ export default function Calculator() {
             <div className={styles.calculator__equation__wrapper}>equation</div>
             <div className={styles.calculator__input__wrapper}>input</div>
             <div className={styles.calculator__buttons__wrapper}>
+
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__function}`}>
+                    AC
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__function}`}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__function}`}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__operator}`}>
+                    b
+                </div>
+
+                {/*  */}
+
                 <div className={styles.buttons__box__wrapper}>
                     b
                 </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__operator}`}>
+                    b
+                </div>
+
+                {/*  */}
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__operator}`}>
+                    b
+                </div>
+
+                {/*  */}
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__operator}`}>
+                    b
+                </div>
+
+                {/*  */}
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={styles.buttons__box__wrapper}>
+                    b
+                </div>
+
+                <div className={`${styles.buttons__box__wrapper} ${styles.button__box__operator}`}>
+                    b
+                </div>
+
+                {/*  */}
+
+
             </div>
         </div>
     );
